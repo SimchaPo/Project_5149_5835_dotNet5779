f02caacc6e8240423cf2f9c153b4164d2801db1d
SimchaPo <45201254+SimchaPo@users.noreply.github.com>
2019-01-09 18:05:59 +0200

    sync

---
+++ UI2/check.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using BE;
+using BL;
+
+namespace UI2
+{
+    /// <summary>
+    /// Interaction logic for check.xaml
+    /// </summary>
+    public partial class check : Window
+    {
+        Trainee trainee;
+        IBL bl;
+
+        public check()
+        {
+            InitializeComponent();
+            trainee = new Trainee();
+            bl = FactoryBL.GetBL();
+            DataContext = trainee;
+            carTypeTraineeComboBox.ItemsSource = Enum.GetValues(typeof(CarType));
+            gearboxTraineeComboBox.ItemsSource = Enum.GetValues(typeof(Gearbox));
+            genderTraineeComboBox.ItemsSource = Enum.GetValues(typeof(Gender));
+            birthDateTraineeDatePicker.DataContext = trainee.BirthDateTrainee;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            bl.addTrainee(trainee);
+        }
+    }
+}
